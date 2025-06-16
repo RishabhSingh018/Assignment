6f4844af5492f49d888405084b4c5c96d53e3a30
RishabhSingh018 <rishi.rklkol@gmail.com>
2025-06-16 11:00:26 +0530

    Add files via upload

---
+++ build/Task.py
@@ -0,0 +1,11 @@
+#Task1
+first_number=int(input('Enter the first Number: '))
+second_number=int(input('Enter the second Number: '))
+print('Addition:',first_number+second_number)
+print('Subtraction: ',first_number-second_number)
+print('Multiplication: ',first_number*second_number)
+print('Division: ',first_number/second_number)
+#Task2
+first_name=input('Enter your First Name: ')
+last_name=input('Enter your Second Name : ')
+print(f"Hello , {first_name} {last_name}! Welcome to the python program")
