3b4c75abb7da6df68f1d2b2fdcb50ffd142b7121
RishabhSingh018 <rishi.rklkol@gmail.com>
2025-06-16 11:28:05 +0530

    Add files via upload

---
+++ 1/Task1.py
@@ -0,0 +1,7 @@
+#Task1
+first_number=int(input('Enter the first Number: '))
+second_number=int(input('Enter the second Number: '))
+print('Addition:',first_number+second_number)
+print('Subtraction: ',first_number-second_number)
+print('Multiplication: ',first_number*second_number)
+print('Division: ',first_number/second_number)
